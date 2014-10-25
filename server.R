aa89afb8f08cf7acd79fb0f19c8ff7d43fd8d809
bungalow-bill <hunter.currie@gmail.com>
2014-10-25 13:21:50 -0600

    Improved menus and layout

--- server.R
+++ server.R
@@ -1,3 +1,5 @@
+# server code for exploration of time series data
+# DHC Oct. 25, 2014
 
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
@@ -6,9 +8,13 @@
 #
 
 library(shiny)
+library(forecast)
 library(fpp)
+library(xts)
 
+# list of datasets - note this needs to match the stuff in ui.R
 dataSets<-list(ds=list(dj,strikes,hsales,eggs,pigs,lynx,beer,elec,usmelec,a10),
+               dnames=list("dj","strikes","hsales","eggs","pigs","lynx","beer","elec","usmelec","a10"),
                xlab=c("Day","Year","Year","Year","Year","Year","Year","Year","Year","Year"),
                ylab=c("DJI","Strike Count","Thousands of Houses","US Dollars","Number Slaughtered","Number Trapped","Megaliters","Billion kWHr","Billion kWHr","Total Prescriptions"),
                title=c("Dow Jones Index","Number of strikes in the US",
@@ -19,7 +25,8 @@ dataSets<-list(ds=list(dj,strikes,hsales,eggs,pigs,lynx,beer,elec,usmelec,a10),
                        "Monthly anti-diabetic drug sales in Australia"))
 
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  
   laggedData<-reactive({
     idx<-as.integer(input$dset)
     tData<-dataSets$ds[[idx]]
@@ -50,7 +57,7 @@ shinyServer(function(input, output) {
   })
   
   output$echo <- renderText({
-    as.integer(input$dset)
+    as.integer(input$auto)
   })
   
   output$rawPlot <- renderPlot({
@@ -69,7 +76,26 @@ shinyServer(function(input, output) {
     plot(tData,xlab=dataSets$xlab[[idx]],ylab=yLab,main=c("Differenced ",dataSets$title[[idx]]))
     
   })
+  
+  output$aboutData<-renderDataTable({
+    as.data.frame(as.xts(dataSets$ds[[as.integer(input$dset)]]))
+  })
 
+  output$tsPlot1<-renderPlot({
+    # tsdisplay of original data
+    idx<-as.integer(input$dset)
+    oData<-dataSets$ds[[idx]]
+    tsdisplay(oData,ylab=dataSets$ylab[[idx]],main=dataSets$title[[idx]])
+  })
+  
+  output$tsPlot2<-renderPlot({
+    # tsdisplay of differenced data
+    idx<-as.integer(input$dset)
+    tData<-laggedData()
+    yLab<-laggedLabel()
+    tsdisplay(tData,ylab=yLab,main=c("Differenced ",dataSets$title[[idx]]))
+  })
+  
   output$acfPlot <- renderPlot({
     # plot the Auto Correlation function for the original and differenced data
     idx<-as.integer(input$dset)
@@ -78,21 +104,37 @@ shinyServer(function(input, output) {
     par(mfrow=c(1,2))
     Acf(oData,main=dataSets$title[[idx]])
     Acf(tData,main=c("Differenced ",dataSets$title[[idx]]))
-    #par(mfrow=C(1,1))
   })
   
-  output$arimaPlot <- renderPlot({
-    # plot the time series data after differencing and logging
+  arimaFit<-reactive({
     idx<-as.integer(input$dset)
-    oData<-dataSets$ds[[idx]]
+    if(input$a_diff == TRUE){
+      oData<-laggedData()
+    }else{
+      oData<-dataSets$ds[[idx]]
+    }
+    
     if(input$auto==TRUE){
       fit <- auto.arima(oData, seasonal=input$aseas)
     } else {
       fit<-  Arima(oData, order = c(input$p, input$d, input$q), seasonal = c(input$P, input$D, input$Q))
+    }   
+    fit
+  })
+  
+  output$arimaPlot <- renderPlot({
+    idx<-as.integer(input$dset)
+    fit<-arimaFit() 
+    incl=as.integer(input$incl)
+    if(incl==0){
+      plot(forecast(fit, h=input$h_int),ylab=dataSets$ylab[[idx]])
+    }else{
+      plot(forecast(fit, h=input$h_int),include=incl,ylab=dataSets$ylab[[idx]])
     }
-    
-    plot(forecast(fit, h=10),include=80)
-
-    
+  })
+  
+  output$residualPlot<-renderPlot({
+    fit<-arimaFit()
+    tsdisplay(residuals(fit),main="Residuals from Arima Forecast")
   })
 })
--- ui.R
+++ ui.R
@@ -1,9 +1,6 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# Shiny user interface definition for Time Series Exploration
+# D. Currie Oct 25, 2014 
 
 library(shiny)
 
@@ -20,52 +17,89 @@ dataList<-c("Dow Jones"=1,
 
 
 shinyUI(fluidPage(
+  tags$head(
+    # Include our custom CSS
+    includeCSS("styles.css")
+  ),
   
   # Application title
   titlePanel("Exploring Time Series Data"),
   
-  # Sidebar with a slider input for number of bins
+  # Sidebar for main controls
   sidebarLayout(
     sidebarPanel(
       selectInput("dset","Select Dataset",choices=dataList),
-      h3("Differencing Options"),
+      helpText("Select a dataset from the list above",
+               "Use the controls below to explore the",
+               "effects of differencing and logging."),
+      h5("Differencing Options"),
       checkboxInput("log","Use Log(x)",FALSE),
       sliderInput("seqd",
                   "Number of sequential lags:",
                   min = 0,
                   max = 3,
                   value = 1),
+      helpText("Sequential lags, or first differences take the",
+               "difference between one observation and the next."),
       sliderInput("seasd",
                   "Number of seasonal lags:",
                   min = 0,
                   max = 3,
                   value = 0),
-      hr(),
-      h3("ARIMA Forecasts"),
-      checkboxInput("auto","Auto ARIMA",TRUE),
-      checkboxInput("aseas","Seasonal",TRUE),
-      numericInput("p","p","0",min=0),
-      numericInput("d","d","0",min=0),
-      numericInput("q","q","0",min=0),
-      numericInput("P","P","0",min=0),
-      numericInput("D","D","0",min=0),
-      numericInput("Q","Q","0",min=0)
+      helpText("Seasonal lags, or M differences take the",
+               "difference between one year and the next.")
+      
     ),
     
-    # Show a plot of the generated distribution
+    # Main panel for results
     mainPanel(
       tabsetPanel(
         tabPanel("Differencing",
-  #       textOutput("echo"),
-          plotOutput("rawPlot"),
-          plotOutput("diffPlot")
+                 #textOutput("echo"),
+                 plotOutput("rawPlot"),
+                 helpText("Using the controls at the left, select sequential and seasonal differences with logs to obtain a stationary result.  A stationary series will have no predictable patterns in the long term."),
+                 plotOutput("diffPlot")
         ),
-      tabPanel("Autocorrelation",
-         plotOutput("acfPlot")),
-      tabPanel("ARIMA Forecasts",
-           plotOutput("arimaPlot"))
+        tabPanel("Autocorrelation",
+                 #         plotOutput("acfPlot"),
+                 plotOutput("tsPlot1"),
+                 helpText("These plots of the Autocorrelation Function",
+                          "are useful for determining the stationarity of a time series.",
+                          "The Acf of stationary data will rapidly decrease to zero, while",
+                          "non-stationary series will decrease more slowly."),
+                 plotOutput("tsPlot2")
+        ),
+        tabPanel("ARIMA Forecasts",
+                 plotOutput("arimaPlot"),
+                 plotOutput("residualPlot"),
+                 absolutePanel(id = "controls", class = "modal", fixed = FALSE, draggable = TRUE,
+                               top = "auto", left = "auto", right = "68%", bottom = "5%",
+                               width = "28%", height = "auto",
+                               checkboxInput("auto","Auto ARIMA",TRUE),
+                               checkboxInput("a_diff","Use differenced data?",FALSE),
+                               numericInput("incl","Data points to show","80",min=0),
+                               numericInput("h_int","Intervals to forecast","10",min=1,max=100),
+                               conditionalPanel(condition="input.auto==0",
+                                                numericInput("p","p","0",min=0),
+                                                numericInput("d","d","0",min=0),
+                                                numericInput("q","q","0",min=0)
+                               ),
+                               checkboxInput("aseas","Seasonal",TRUE),
+                               conditionalPanel(condition="input.aseas==1 && input.auto==0",
+                                                numericInput("P","P","0",min=0),
+                                                numericInput("D","D","0",min=0),
+                                                numericInput("Q","Q","0",min=0)
+                               )
+                 )
+                 
+        ),
+        tabPanel("About",
+                 includeHTML("doc.html")
+                 #               dataTableOutput("aboutData")
+        )
       )
     )
+    
   )
 )
 )
